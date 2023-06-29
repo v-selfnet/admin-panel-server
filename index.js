8a82f4a814be7c7b31f190c63b845604a8f9943f
Ashraful Haque <vselfnet@gmail.com>
2023-06-29 17:50:54 +0200

    Implement Login, Register & SocialLogin done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 3000;
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // console.log(process.env.DB_PASS)
 
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
   }
 });
 
- const run = async () => {
+const run = async () => {
   try {
     await client.connect();
 
@@ -33,17 +33,39 @@ const client = new MongoClient(uri, {
     // create DB & store all users 
     // from Register.jsx & SocialLogin.jsx
     app.post('/users', async (req, res) => {
-        const user = req.body;
-        // const existingUser = await usersCollection.findOne({email: user.email})
-        if(await usersCollection.findOne({email: user.email})) return;
-        const result = await usersCollection.insertOne(user);
-        res.send(result);
+      const user = req.body;
+      // const query = {email: user.email}
+      // const existingUser = await usersCollection.findOne(query)
+      // if(existingUser) return res.send({ message: 'user already exist' });
+      if (await usersCollection.findOne({ email: user.email }))
+        return res.send({ message: '[ SERVER ] User Already Exist. Void Save to Database' });
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
     })
-    
-    // from ManageUsers.jsx
+
+    // display users info in UI from ManageUsers.jsx
     app.get('/users', async (req, res) => {
-        const result = await usersCollection.find().toArray();
-        res.send(result);
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+
+    // update user info. become an Admin
+    // Manageusers.jsx
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updateUser = {
+        $set: { role: 'admin' }
+      }
+      const result = usersCollection.updateOne(filter, updateUser);
+      res.send(result)
+    })
+
+    // delete user from ManageUser.jsx
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const result = await usersCollection.deleteOne({ _id: new ObjectId(id) })
+      res.send(result)
     })
 
     // ping success to mongoDB
@@ -57,9 +79,9 @@ run().catch(console.dir);
 
 // testing serve setup
 app.get('/', (req, res) => {
-    res.send('Admin Panel Server is Running')
+  res.send('Admin Panel Server is Running')
 })
 
 app.listen(port, () => {
-    console.log(`Admin Panel Server is Running on port: ${port}`)
+  console.log(`Admin Panel Server is Running on port: ${port}`)
 })
